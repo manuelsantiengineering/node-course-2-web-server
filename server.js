68bfb189c18a08cc88ee1308da78b6fe3268f95b
manuelsantiengineering <manuelsantiengineering@gmail.com>
2017-09-22 20:50:23 -0400

    Added the weather html

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 /*
-  To push into horok:
+  To push into heroku:
   1) git add .
   2) git commit -m 'message for git repository'
   3) git push origin
@@ -12,8 +12,8 @@ const axios = require('axios');
 const express = require('express');
 const hbs = require('hbs');//handlebars
 const fs = require('fs');
+var Promise = require('promise');
 
-const geocode = require('./weather/geocode.js');
 const weather = require('./weather/weather.js');
 
 const port = process.env.PORT || 3000; //If proces does not find any port, use 3000
@@ -41,92 +41,38 @@ hbs.registerHelper('getCurrentYear', () => {
 });
 hbs.registerHelper('screamIt', (text)=> {
   return text.toUpperCase();
-})
-app.get('/', (request, response) => {
-
-  var locationResults;
-  var weatherResults;
-  geocode.geocodeAddress(address, (errorMessage, locationResults) =>{
-    if(errorMessage){
-      console.log(errorMessage);
-    }else {
-      locationResults.pageTitle = 'Home Page';
-      locationResults.message = 'This is my home page';
-      
-      response.render('home.hbs', locationResults);
-
-      console.log(JSON.stringify(locationResults, undefined, 2));
-      var weatherURL = `https://api.darksky.net/forecast/838420834287d2da59289c6eeaf05a1d/${locationResults.latitude},${locationResults.longitude}`;
-      weather.restGETWeather(weatherURL, (errorMessage, weatherResults) => {
-        if(errorMessage){
-          console.log(errorMessage);
-        }else {
-          // weatherResults.pageTitle = 'Home Page';
-          // weatherResults.message = 'This is the home page';
-          console.log(JSON.stringify(weatherResults, undefined, 2));
-          // response.render('home.hbs', {
-          //   pageTitle: 'Home Page',
-          //   message:'This is the home page'
-          // });
-        }
-      });
-
-    }
-  });
-
-  // response.render('home.hbs', {
-  //   pageTitle: 'Home Page',
-  //   message:'This is the home page'
-  // });
-
 });
 
 
-// app.get('/', (req, res) => {
-//
-//   var locationResults;
-//   var weatherResults;
-//   var encodedAddress = `${encodeURIComponent(address)}`;
-//   var geocodeURL = 'https://maps.googleapis.com/maps/api/geocode/json?address='+encodedAddress;
-//
-//   axios.get(geocodeURL)
-//   .then(
-//     (response) => {
-//       if(response.data.status === 'ZERO_RESULTS'){
-//         throw new Error('Error: Unable to find the address.');
-//       }else if (response.data.status === 'OK') {
-//             console.log(JSON.stringify(response.data.results[0].geometry.location, undefined, 5));
-//             var location = response.data.results[0].geometry.location;
-//             var weatherData = weather.getWeatherResults(location.lat, location.lng)
-//             .then( (data) => {
-//               console.log(data);
-//               data.pageTitle = 'Home Page';
-//               data.message = 'This is the home page';
-//               res.render('home.hbs',data);
-//             }, (err) => {
-//               console.log(err);
-//             });
-//             // console.log(weatherData);
-//             // weatherData.pageTitle = 'Home Page';
-//             // weatherData.message = 'This is the home page';
-//             // res.render('home.hbs',weatherData);
-//
-//       }
-//     }
-//   )
-//   .catch(
-//     (error) => {
-//       if(error.code === 'ENOTFOUND'){
-//         console.log('Error: Unable to connect to API servers.');
-//       }else{
-//         console.log(error.message);
-//       }
-//     }
-//   );
-//
-//
-// });
+app.get('/', (request, response) => {
+  response.render('home.hbs', {
+    pageTitle: 'Home Page',
+    message:'This is the home page'
+  });
+});
 
+app.get('/weather', (request, response) => {
+  weather.getAddress(address)
+  .then( (locationData) => {
+      console.log(`Afuera: ${JSON.stringify(locationData)}`);
+      var wheatherData = weather.getWeather(locationData.latitude, locationData.longitude)
+      wheatherData.address = locationData.address;
+      return (wheatherData);
+  })
+  .then( (wheaterData) => {
+    wheaterData.pageTitle = 'Weather Page';
+    wheaterData.message = 'This is the weather page';
+    console.log(wheaterData);
+    response.render('weather.hbs', wheaterData);
+  })
+  .catch( (error) => {
+    console.log(error);
+    response.render('error.hbs', {
+      pageTitle: 'Error!',
+      message:error
+    });
+  });
+});
 
 app.get('/about', (request, response) => {
   response.render('about.hbs', {
--- weather/weather.js
+++ weather/weather.js
@@ -1,42 +1,57 @@
-
 const axios = require('axios');
 //DarkSkyAPI secret key: 838420834287d2da59289c6eeaf05a1d
 //URL: https://api.darksky.net/forecast/838420834287d2da59289c6eeaf05a1d/LATITUDE,LONGITUDE
 
-var restGETWeather = (weatherURL) => {
-  return (axios.get(weatherURL) );
-};
+var getAddress = (address) => {
+  var encodedAddress = `${encodeURIComponent(address)}`;
+  var fetchURL = 'https://maps.googleapis.com/maps/api/geocode/json?address='+encodedAddress;
 
-var getWeatherResults = (latitude, longitude) => {
+  return new Promise( (resolve, reject) => {
+      axios.get(fetchURL)
+      .then( (response) => {
+          if (response.data.status === "OK" && response.data.status !== "ZERO_RESULTS") {
+                var coordinates = {
+                  latitude:response.data.results[0].geometry.location.lat,
+                  longitude:response.data.results[0].geometry.location.lng,
+                  address:response.data.results[0].formatted_address
+                };
+                resolve(coordinates);
+          } else {
+            reject('Error: Unable to find the location.');
+          }
+        }
+      );
+  });
+}
+
+
+var getWeather = (latitude, longitude) => {
   var weatherURL = `https://api.darksky.net/forecast/838420834287d2da59289c6eeaf05a1d/${latitude},${longitude}`;
-  var weatherGet = restGETWeather(weatherURL);
-  var weatherData = {result:'OK'};
 
-  weatherGet.then(
-      (response) => {
+  return new Promise( (resolve, reject) => {
+    axios.get(weatherURL)
+    .then( (response) => {
         if (response.data.latitude === latitude && response.data.longitude === longitude) {
-          weatherData.temperature = response.data.currently.temperature;
-          weatherData.apparentTemperature = response.data.currently.apparentTemperature;
-          weatherData.precipType = response.data.currently.precipType;
-          weatherData.precipProbability = response.data.currently.precipProbability;
-          weatherData.summary = response.data.currently.summary;
-          console.log(response.data.currently.summary);
-          return weatherData;
+              var weatherData = {
+                temperature:response.data.currently.temperature,
+                apparentTemperature:response.data.currently.apparentTemperature,
+                precipType:response.data.currently.precipType,
+                precipProbability:response.data.currently.precipProbability,
+                summary:response.data.currently.summary,
+                fetchURL:weatherURL
+              };
+              resolve(weatherData);
         }else{
-          throw new Error('Error: Unable to find the weather.');
+          reject('Error: Unable to find the weather.');
         }
       }
-    )
-    .catch(
-      (error) => {
-        console.log(error.message);
-        weatherData.results = error.message;
-        return weatherData;
-      }
     );
-};
+  });
+}
+
+
 
 module.exports = {
-  restGETWeather,
-  getWeatherResults
+  getAddress,
+  getWeather
 };
